b207280a4fe813de713158abd4212d5ac1c54742
bellatorioxd <107697714+bellatorioxd@users.noreply.github.com>
2023-04-12 00:07:46 +0300

    Add files via upload

--- isw_data.py
+++ isw_data.py
@@ -6,8 +6,21 @@ import re
 start_date = pd.Timestamp("2022-02-24")
 end_date = pd.Timestamp("2023-01-25")
 data = []
+pos_of_missing_url=0
+
+missing_urls = [
+    "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-initial-russian-offensive-campaign-assessment",
+    "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-25-2022",
+    "https://understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-26",
+    "https://www.understandingwar.org/backgrounder/russia-ukraine-warning-update-russian-offensive-campaign-assessment-february-27",
+    "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-february-28-2022",
+    "https://www.understandingwar.org/backgrounder/russian-campaign-assessment-may-5",
+    "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-update-july-11",
+    "https://www.understandingwar.org/backgrounder/russian-offensive-campaign-assessment-august-12-0"
+]
 
 for date in pd.date_range(start_date, end_date):
+
     year = date.year
     month = date.strftime("%B").lower()
     day = date.strftime("%#d")
@@ -20,8 +33,18 @@ for date in pd.date_range(start_date, end_date):
     try:
         response = requests.get(url)
         if response.status_code != 200:
-            print(f"{date} data is not available.")
-            continue
+            print(f"{date} data is not available. Searching internally...")
+            if pos_of_missing_url <8:
+                response = requests.get(missing_urls[pos_of_missing_url])
+                pos_of_missing_url+=1
+                if response.status_code != 200:
+                    print(f"{date} data is not available.")
+                    continue
+                else:
+                    print(f"Info for {date} found")
+            else:
+                print(f"No link founded for {date}")
+                continue
         soup = bs(response.content, 'html.parser')
         description = soup.find("div", {"class": "field-name-body"}).text
         description = description.replace(" dot ", ".")
@@ -34,6 +57,7 @@ for date in pd.date_range(start_date, end_date):
         pattern = re.compile('[^A-Za-z0-9\s]')
         description = pattern.sub("", description)
         description = re.sub(r'^.*?(est|et)', '', description, flags=re.DOTALL)
+        description = description.replace("click here to see isws interactive map of the russian invasion of ukraine this map is updated daily alongside the static maps present in this report", "")
         data.append({"Date": date.strftime("%B-%d-%Y").lower(), "Description": description})
     except (requests.exceptions.RequestException, KeyError, TypeError) as error:
         print(f"{error} for {date}")
